bbfe78110455be44967eae6cb4e05aed899b302d
emanuel01251 <jameroemanuel01251@gmail.com>
2022-08-24 14:05:20 +0800

    first commit

---
+++ resources/views/post/followers.blade.php
@@ -0,0 +1,14 @@
+<x-app-layout>
+    
+<div class="container px-3 mx-auto grid bg-gray-100">
+<style>
+	input, textarea, button, select, a { -webkit-tap-highlight-color: rgba(0,0,0,0); }
+	button:focus{ outline:0 !important; } }
+	
+</style>
+
+    <livewire:posts.view :type="'followers'" />
+    
+</div>
+            
+</x-app-layout>
